649bc724bebd060a554ba0c54aaa7d37b2d1c766
Yinner Chilito <yinnermedina30@gmail.com>
2024-04-13 13:29:14 -0500

    Ajuste validación Schema Formulario de usuarios

--- src/dashboard/users/ElementsUserForm.ts
+++ src/dashboard/users/ElementsUserForm.ts
@@ -43,33 +43,33 @@ export const formFields: Record<string, CustomFormField> = {
 
 export const formSchema = z.object({
   first_name: z
-    .string({ required_error: 'El campo nombre es obligatorio' })
+    .string({ required_error: 'El nombre es obligatorio' })
     .min(2, {
       message: 'El nombre debe tener al menos 2 caracteres',
     })
     .max(100, { message: `El nombre no debe exceder los 100 caracteres` }),
   last_name: z
-    .string({ required_error: 'El campo apellido es obligatorio' })
+    .string({ required_error: 'El apellido es obligatorio' })
     .min(4, {
       message: 'El apellido debe tener al menos 4 caracteres',
     })
     .max(100, { message: `El apellido no debe exceder los 100 caracteres` }),
   email: z
-    .string({ required_error: 'El campo correo electrónico es obligatorio' })
+    .string({ required_error: 'El correo electrónico es obligatorio' })
     .email('El correo electrónico es incorrecto')
     .max(100, {
       message: `El correo electrónico no debe superar los 100 caracteres`,
     }),
   cell_phone_number: z
     .string({
-      required_error: 'El número de teléfono es obligatorio',
+      required_error: 'El número celular es obligatorio',
     })
     .refine(
       data => {
         return /^3\d{9}$/.test(data);
       },
       {
-        message: 'El número de teléfono es incorrecto.',
+        message: 'El número celular es incorrecto',
       },
     ),
   password: z
