e49d2e7253168a59ce54881af6cc4c147c47f5cc
Yinner Chilito <92528747+YinnerC30@users.noreply.github.com>
2024-06-29 11:40:30 -0500

    funcionalidad ver información de venta

--- src/modules/sales/routes/Routes.tsx
+++ src/modules/sales/routes/Routes.tsx
@@ -1,6 +1,7 @@
 import { SaleModule } from "../components";
 import { CreateSale } from "../components/CreateSale";
 import { ModifySale } from "../components/ModifySale";
+import { ViewSale } from "../components/ViewSale";
 
 const saleRoutes = {
   path: "sales",
@@ -16,6 +17,10 @@ const saleRoutes = {
     {
       path: "modify/:id",
       element: <ModifySale />,
+    },
+    {
+      path: "view/:id",
+      element: <ViewSale />, //TODO: Cambiar este componente por el correcto
     },
   ],
 };
