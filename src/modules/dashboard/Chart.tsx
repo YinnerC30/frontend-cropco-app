dbb899ae0b8d85f965ebdb20e9e456e572edf553
Yinner Chilito <92528747+YinnerC30@users.noreply.github.com>
2024-07-31 16:59:05 -0500

    agregación de estilos al grafico

--- src/modules/dashboard/Chart.tsx
+++ src/modules/dashboard/Chart.tsx
@@ -1,15 +1,28 @@
 "use client";
 
-import { Bar, BarChart } from "recharts";
-
 import { ChartContainer } from "@/components/ui/chart";
 import { chartConfig } from "./ChartConfig";
 import { chartData } from "./ChartData";
+import { Bar, BarChart, CartesianGrid, XAxis } from "recharts";
+import { ChartTooltip, ChartTooltipContent } from "@/components/ui/chart"
+import { ChartLegend, ChartLegendContent } from "@/components/ui/chart"
+
+
 
 export function Chart() {
   return (
     <ChartContainer config={chartConfig} className="max-h-[200px] w-full">
       <BarChart accessibilityLayer data={chartData}>
+        <CartesianGrid vertical={false} />
+        <XAxis
+          dataKey="month"
+          tickLine={false}
+          tickMargin={10}
+          axisLine={false}
+          tickFormatter={(value) => value.slice(0, 3)}
+        />
+         <ChartTooltip content={<ChartTooltipContent />} />
+         <ChartLegend content={<ChartLegendContent />} />
         <Bar dataKey="desktop" fill="var(--color-desktop)" radius={4} />
         <Bar dataKey="mobile" fill="var(--color-mobile)" radius={4} />
       </BarChart>
