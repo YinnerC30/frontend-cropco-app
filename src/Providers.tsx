8b385f19bc61b313549e9491b868194de0eeef85
Yinner Chilito <92528747+YinnerC30@users.noreply.github.com>
2024-10-07 12:15:05 -0500

    refactor: 💡 nombre de providers de la app

--- src/Providers.tsx
+++ src/Providers.tsx
@@ -1,6 +1,6 @@
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
-import { Provider } from "react-redux";
+import { Provider as ReduxProvider } from "react-redux";
 import { RouterProvider } from "react-router-dom";
 import { ThemeProvider } from "./modules/core/components";
 import { store } from "./redux/store";
@@ -14,16 +14,14 @@ interface Props {
 
 export const ProvidersApp = ({ children }: Props) => {
   return (
-    <>
-      <Provider store={store}>
-        <QueryClientProvider client={queryClient}>
-          <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
-            <RouterProvider router={Router} />
-            {children}
-          </ThemeProvider>
-          <ReactQueryDevtools />
-        </QueryClientProvider>
-      </Provider>
-    </>
+    <ReduxProvider store={store}>
+      <QueryClientProvider client={queryClient}>
+        <ThemeProvider defaultTheme="system" storageKey="vite-ui-theme">
+          <RouterProvider router={Router} />
+          {children}
+        </ThemeProvider>
+        <ReactQueryDevtools />
+      </QueryClientProvider>
+    </ReduxProvider>
   );
 };
