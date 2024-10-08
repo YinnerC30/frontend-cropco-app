f66fda8df9a22b2c3f581778ca7b39536762c9b3
Yinner Chilito <92528747+YinnerC30@users.noreply.github.com>
2024-10-08 09:26:46 -0500

    refactor: 💡 Creación de usuario con permisos

--- src/modules/users/components/FormUser.tsx
+++ src/modules/users/components/FormUser.tsx
@@ -1,4 +1,4 @@
-import { Button, Input, ScrollArea } from "@/components";
+import { Button, Input, ScrollArea } from '@/components';
 import {
   Form,
   FormControl,
@@ -7,15 +7,16 @@ import {
   FormItem,
   FormLabel,
   FormMessage,
-} from "@/components/ui/form";
-import { ButtonsForm } from "@/modules/core/components/ButtonsForm";
-import { FormFieldInput } from "@/modules/core/components/form/FormFieldInput";
-import { EyeClosedIcon, EyeOpenIcon } from "@radix-ui/react-icons";
-import { useUserForm } from "../hooks/useUserForm";
-import { formFieldsUser } from "../utils";
-import { useEffect } from "react";
-import { useNavigate } from "react-router-dom";
-import { FormProps } from "@/modules/core/interfaces/FormProps";
+} from '@/components/ui/form';
+import { ButtonsForm } from '@/modules/core/components/ButtonsForm';
+import { FormFieldInput } from '@/modules/core/components/form/FormFieldInput';
+import { FormProps } from '@/modules/core/interfaces/FormProps';
+import { EyeClosedIcon, EyeOpenIcon } from '@radix-ui/react-icons';
+import { useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { useUserForm } from '../hooks/useUserForm';
+import { formFieldsUser } from '../utils';
+import { UpdateUserActions } from './UpdateUserActions';
 
 export const FormUser = ({
   onSubmit,
@@ -32,111 +33,121 @@ export const FormUser = ({
 
   return (
     <div className="flex flex-col items-center w-full ">
-      <ScrollArea type="auto" className="h-[75vh] w-2/4">
-        <Form {...form}>
-          <form
-            onSubmit={form.handleSubmit(onSubmit)}
-            id="formUser"
-            className="flex flex-row flex-wrap gap-4 p-2 justify-stretch"
-          >
-            <FormFieldInput
-              control={form.control}
-              description={formFieldsUser.first_name.description}
-              label={formFieldsUser.first_name.label}
-              name={"first_name"}
-              placeholder={formFieldsUser.first_name.placeholder}
-              readOnly={readOnly}
-            />
-            <FormFieldInput
-              control={form.control}
-              description={formFieldsUser.last_name.description}
-              label={formFieldsUser.last_name.label}
-              name={"last_name"}
-              placeholder={formFieldsUser.last_name.placeholder}
-              readOnly={readOnly}
-            />
-            <FormFieldInput
-              control={form.control}
-              description={formFieldsUser.email.description}
-              label={formFieldsUser.email.label}
-              name={"email"}
-              placeholder={formFieldsUser.email.placeholder}
-              readOnly={readOnly}
-            />
-            <FormFieldInput
-              className="w-full"
-              control={form.control}
-              description={formFieldsUser.cell_phone_number.description}
-              label={formFieldsUser.cell_phone_number.label}
-              name={"cell_phone_number"}
-              placeholder={formFieldsUser.cell_phone_number.placeholder}
-              readOnly={readOnly}
-            />
+      <div className="flex items-center gap-2">
+        <ScrollArea type="auto" className="h-[70vh] w-2/4 ">
+          <h3 className="text-xl ">Datos personales:</h3>
 
-            {!readOnly && (
-              <>
-                <FormField
-                  control={form.control}
-                  name={`password.password1`}
-                  render={({ field }) => (
-                    <FormItem>
-                      <FormLabel>{formFieldsUser.password1.label}</FormLabel>
-                      <div className="flex gap-2">
-                        <FormControl>
-                          <Input
-                            className="w-56"
-                            {...field}
-                            type={showPassword ? "text" : "password"}
-                            readOnly={readOnly}
-                          />
-                        </FormControl>
-                        <Button onClick={(e) => togglePasswordVisibility(e)}>
-                          {showPassword ? <EyeOpenIcon /> : <EyeClosedIcon />}
-                        </Button>
-                      </div>
-                      <FormDescription>
-                        {formFieldsUser.password1.description}
-                      </FormDescription>
-                      <FormMessage />
-                    </FormItem>
-                  )}
-                />
-                <FormField
-                  control={form.control}
-                  name={`password.password2`}
-                  render={({ field }) => (
-                    <FormItem>
-                      <FormLabel>{formFieldsUser.password2.label}</FormLabel>
-                      <div className="flex gap-2">
-                        <FormControl>
-                          <Input
-                            className="w-56"
-                            {...field}
-                            type={showPassword ? "text" : "password"}
-                            readOnly={readOnly}
-                          />
-                        </FormControl>
-                        <Button onClick={(e) => togglePasswordVisibility(e)}>
-                          {showPassword ? <EyeOpenIcon /> : <EyeClosedIcon />}
-                        </Button>
-                      </div>
-                      <FormDescription>
-                        {formFieldsUser.password2.description}
-                      </FormDescription>
-                      <FormMessage />
-                    </FormItem>
-                  )}
-                />
-              </>
-            )}
-          </form>
-        </Form>
-      </ScrollArea>
+          <Form {...form}>
+            <form
+              onSubmit={form.handleSubmit(onSubmit)}
+              id="formUser"
+              className="flex flex-row flex-wrap gap-4 p-2 justify-stretch"
+            >
+              <FormFieldInput
+                control={form.control}
+                description={formFieldsUser.first_name.description}
+                label={formFieldsUser.first_name.label}
+                name={'first_name'}
+                placeholder={formFieldsUser.first_name.placeholder}
+                readOnly={readOnly}
+              />
+              <FormFieldInput
+                control={form.control}
+                description={formFieldsUser.last_name.description}
+                label={formFieldsUser.last_name.label}
+                name={'last_name'}
+                placeholder={formFieldsUser.last_name.placeholder}
+                readOnly={readOnly}
+              />
+              <FormFieldInput
+                control={form.control}
+                description={formFieldsUser.email.description}
+                label={formFieldsUser.email.label}
+                name={'email'}
+                placeholder={formFieldsUser.email.placeholder}
+                readOnly={readOnly}
+              />
+              <FormFieldInput
+                // className="w-full"
+                control={form.control}
+                description={formFieldsUser.cell_phone_number.description}
+                label={formFieldsUser.cell_phone_number.label}
+                name={'cell_phone_number'}
+                placeholder={formFieldsUser.cell_phone_number.placeholder}
+                readOnly={readOnly}
+              />
+
+              {!readOnly && (
+                <>
+                  <FormField
+                    control={form.control}
+                    name={`password.password1`}
+                    render={({ field }) => (
+                      <FormItem className="w-full">
+                        <FormLabel>{formFieldsUser.password1.label}</FormLabel>
+                        <div className="flex gap-2">
+                          <FormControl>
+                            <Input
+                              className="w-56"
+                              {...field}
+                              type={showPassword ? 'text' : 'password'}
+                              readOnly={readOnly}
+                            />
+                          </FormControl>
+                          <Button onClick={(e) => togglePasswordVisibility(e)}>
+                            {showPassword ? <EyeOpenIcon /> : <EyeClosedIcon />}
+                          </Button>
+                        </div>
+                        <FormDescription>
+                          {formFieldsUser.password1.description}
+                        </FormDescription>
+                        <FormMessage />
+                      </FormItem>
+                    )}
+                  />
+                  <FormField
+                    control={form.control}
+                    name={`password.password2`}
+                    render={({ field }) => (
+                      <FormItem className="w-full">
+                        <FormLabel>{formFieldsUser.password2.label}</FormLabel>
+                        <div className="flex gap-2">
+                          <FormControl>
+                            <Input
+                              className="w-56"
+                              {...field}
+                              type={showPassword ? 'text' : 'password'}
+                              readOnly={readOnly}
+                            />
+                          </FormControl>
+                          <Button onClick={(e) => togglePasswordVisibility(e)}>
+                            {showPassword ? <EyeOpenIcon /> : <EyeClosedIcon />}
+                          </Button>
+                        </div>
+                        <FormDescription>
+                          {formFieldsUser.password2.description}
+                        </FormDescription>
+                        <FormMessage />
+                      </FormItem>
+                    )}
+                  />
+                </>
+              )}
+            </form>
+          </Form>
+        </ScrollArea>
+
+        <ScrollArea className="w-2/4 h-[70vh] mt-2">
+          <h3 className="text-xl ">Permisos:</h3>
+          <UpdateUserActions className={'mt-2'} />
+        </ScrollArea>
+      </div>
+
       {!readOnly && (
         <ButtonsForm
           isPending={isPending ?? false}
-          formId={"formUser"}
-          className={"flex w-48 gap-2 mt-2"}
+          formId={'formUser'}
+          className={'flex w-48 gap-2 mt-2'}
         />
       )}
 
--- src/modules/users/components/UpdateUserActions.tsx
+++ src/modules/users/components/UpdateUserActions.tsx
@@ -1,17 +1,14 @@
-import { Button } from "@/components";
 import {
   Card,
   CardContent,
   CardFooter,
   CardHeader,
   CardTitle,
-} from "@/components/ui/card";
-import { ScrollArea } from "@/components/ui/scroll-area";
-import { Switch } from "@/components/ui/switch";
-import { CapitalizeFirstWord } from "@/modules/authentication/helpers/CapitalizeFirstWord";
-import { ErrorLoading, Loading } from "@/modules/core/components";
-import { useGetAllModules } from "@/modules/core/hooks/useGetAllModules";
-import { useNavigate } from "react-router-dom";
+} from '@/components/ui/card';
+import { Switch } from '@/components/ui/switch';
+import { CapitalizeFirstWord } from '@/modules/authentication/helpers/CapitalizeFirstWord';
+import { ErrorLoading, Loading } from '@/modules/core/components';
+import { useGetAllModules } from '@/modules/core/hooks/useGetAllModules';
 
 const ModuleAction = ({ name_action }: any) => {
   return (
@@ -22,10 +19,10 @@ const ModuleAction = ({ name_action }: any) => {
   );
 };
 
-export const UpdateUserActions = () => {
+export const UpdateUserActions = ({className}:any) => {
   const { data, isLoading, isError } = useGetAllModules();
 
-  const navigate = useNavigate();
+  // const navigate = useNavigate();
 
   if (isLoading) {
     return <Loading />;
@@ -35,49 +32,34 @@ export const UpdateUserActions = () => {
     return <ErrorLoading />;
   }
 
-  const handleCancel = () => {
-    navigate("../all");
-  };
-
-  
-  
+  // const handleCancel = () => {
+  //   navigate('../all');
+  // };
 
   return (
-    <>
-      <ScrollArea className="w-full h-[80vh] mt-2">
-        {/* <div className="flex flex-wrap justify-evenly"> */}
-        {/* <div> */}
-        {data.map(({ label, actions, name }: any) => {
-          return (
-            <Card key={name} className="w-2/4 mb-2">
-              <CardHeader className="border-b">
-                <CardTitle className="underline capitalize underline-offset-4">
-                  {label}
-                </CardTitle>
-              </CardHeader>
-              <CardContent className="flex flex-col flex-wrap gap-4 m-2 rounded-md">
-                {actions.map((act: any) => {
-                  return (
-                    <div key={act.name}>
-                      <ModuleAction name_action={CapitalizeFirstWord(act.name)} />
-                    </div>
-                  );
-                })}
-              </CardContent>
-              <CardFooter>
-                {/* <Button variant={"link"}>Marcar todas</Button> */}
-                {/* <Button variant={"ghost"}>Desmarcar todas</Button> */}
-              </CardFooter>
-            </Card>
-          );
-        })}
-        {/* </div> */}
-        {/* </div> */}
-      </ScrollArea>
-      <div className="flex gap-2 mt-4 ">
-        <Button>Guardar</Button>
-        <Button onClick={handleCancel}>Cancelar</Button>
-      </div>
-    </>
+    <div className={className}>
+      {data.map(({ label, actions, name }: any) => {
+        return (
+          <Card key={name} className="w-2/4 mb-2">
+            <CardHeader className="border-b">
+              <CardTitle className="capitalize ">{label}</CardTitle>
+            </CardHeader>
+            <CardContent className="flex flex-col flex-wrap gap-4 m-2 rounded-md">
+              {actions.map((act: any) => {
+                return (
+                  <div key={act.name}>
+                    <ModuleAction name_action={CapitalizeFirstWord(act.name)} />
+                  </div>
+                );
+              })}
+            </CardContent>
+            <CardFooter>
+              {/* <Button variant={"link"}>Marcar todas</Button> */}
+              {/* <Button variant={"ghost"}>Desmarcar todas</Button> */}
+            </CardFooter>
+          </Card>
+        );
+      })}
+    </div>
   );
 };
