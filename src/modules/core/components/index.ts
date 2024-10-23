f7ec418265ada3a4f76c4932e53c8701c58e3140
Yinner Chilito <92528747+YinnerC30@users.noreply.github.com>
2024-10-23 08:52:52 -0500

    fix: 🐛 importaciones de componentes

--- src/modules/core/components/index.ts
+++ src/modules/core/components/index.ts
@@ -1,17 +1,16 @@
-export { BasicSearchBar as SearchBar } from "./search-bar/BasicSearchBar";
+export { BasicSearchBar as SearchBar } from './search-bar/BasicSearchBar';
 
-export { ActionsTable } from "./table/ActionsTable";
-export { ButtonHeaderTable } from "./table/ButtonHeaderTable";
-export { DataTable } from "./table/DataTable";
-export { DataTablePagination } from "./table/DataTablePagination";
-export { FilterByColumn } from "./table/FilterByColumn";
+export { ActionsTable } from './table/ActionsTable';
+export { ButtonHeaderTable } from './table/ButtonHeaderTable';
+export { DataTable } from './table/DataTable';
+export { DataTablePagination } from './table/DataTablePagination';
+export { FilterByColumn } from './table/FilterByColumn';
 
-export { ButtonCancelRegister } from "./ButtonCancelRegister";
-export { DeleteRecord } from "./DeleteRecord";
-export { ErrorLoading } from "./ErrorLoading";
-export { Home } from "../../../Home";
-export { Loading } from "./Loading";
-export { ModeToggle } from "./ModeToggle";
+export { ButtonCancelRegister } from './ButtonCancelRegister';
+export { DeleteRecord } from './DeleteRecord';
+export { ErrorLoading } from './ErrorLoading';
+export { Home } from '../../../Home';
+export { Loading } from './Loading';
 
-export { ThemeProvider } from "./ThemeProvider";
-export { ToolTipTemplate } from "./ToolTipTemplate";
+export { ThemeProvider } from './ThemeProvider';
+export { ToolTipTemplate } from './ToolTipTemplate';
