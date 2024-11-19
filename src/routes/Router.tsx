9a3583de833d308073145baeccd1ae83c9f18d55
Yinner Chilito <92528747+YinnerC30@users.noreply.github.com>
2024-11-19 07:41:03 -0500

    refactor: 💡 WIP organización de file system

--- src/routes/Router.tsx
+++ src/routes/Router.tsx
@@ -1,24 +1,9 @@
-import ErrorPage from '@/ErrorPage';
-import { authenticationRoutes } from '@/modules/authentication/routes/Routes';
-
-import { clientRoutes } from '@/modules/clients/routes/Routes';
-import { consumptionRoutes } from '@/modules/consumption/routes/Routes';
-
-import { cropRoutes } from '@/modules/crops/routes/Routes';
-import { employeeRoutes } from '@/modules/employees/routes/Routes';
-
-import { dashboardRoutes } from '@/modules/dashboard/routes/Routes';
-import { harvestRoutes } from '@/modules/harvests/routes/Routes';
-import { paymentsRoutes } from '@/modules/payments/routes/Routes';
-import { saleRoutes } from '@/modules/sales/routes/Routes';
-import { shoppingRoutes } from '@/modules/shopping/routes/Routes';
-import { supplierRoutes } from '@/modules/suppliers/routes/Routes';
-import { supplyRoutes } from '@/modules/supplies/routes/Routes';
-import { userRoutes } from '@/modules/users/routes/userRoutes';
-import { workRoutes } from '@/modules/work/routes/Routes';
-
 import { LandingPage } from '@/components/common/LandingPage';
+import ErrorPage from '@/ErrorPage';
 import AuthenticationLayout from '@/modules/authentication/components/AuthenticationLayout';
+import { authenticationRoutes } from '@/modules/authentication/routes/Routes';
+import { dashboardRoutes } from '@/modules/dashboard/routes/Routes';
+import { userRoutes } from '@/modules/users/routes/userRoutes';
 import { createBrowserRouter, Navigate } from 'react-router-dom';
 import { HomeLayout } from '../HomeLayout';
 import { RoutesController } from './components/RoutesController';
@@ -50,17 +35,17 @@ export const Router = createBrowserRouter([
           { index: true, element: <Navigate to={'dashboard'} /> },
           dashboardRoutes,
           userRoutes,
-          cropRoutes,
-          clientRoutes,
-          harvestRoutes,
-          employeeRoutes,
-          supplierRoutes,
-          supplyRoutes,
-          saleRoutes,
-          workRoutes,
-          paymentsRoutes,
-          shoppingRoutes,
-          consumptionRoutes,
+          // cropRoutes,
+          // clientRoutes,
+          // harvestRoutes,
+          // employeeRoutes,
+          // supplierRoutes,
+          // supplyRoutes,
+          // saleRoutes,
+          // workRoutes,
+          // paymentsRoutes,
+          // shoppingRoutes,
+          // consumptionRoutes,
         ],
       },
     ],
--- src/routes/components/RoutesNavBar.tsx
+++ src/routes/components/RoutesNavBar.tsx
@@ -1,18 +1,4 @@
-import {
-  Blocks,
-  BookUser,
-  Cable,
-  CircleDollarSign,
-  Contact,
-  CreditCard,
-  Leaf,
-  Monitor,
-  Pickaxe,
-  PillIcon,
-  ShoppingBagIcon,
-  Tractor,
-  User,
-} from 'lucide-react';
+import { Monitor, User } from 'lucide-react';
 
 export interface Route {
   label: string;
@@ -34,70 +20,70 @@ export const routes: Route[] = [
     Icon: <User />,
     name_module: 'users',
   },
-  {
-    label: 'Empleados',
-    path: 'employees/view/all',
-    Icon: <Contact />,
-    name_module: 'employees',
-  },
-  {
-    label: 'Cultivos',
-    path: 'crops/view/all',
-    Icon: <Leaf />,
-    name_module: 'crops',
-  },
-  {
-    label: 'Clientes',
-    path: 'clients/view/all',
-    Icon: <BookUser />,
-    name_module: 'clients',
-  },
-  {
-    label: 'Proveedores',
-    path: 'suppliers/view/all',
-    Icon: <Blocks />,
-    name_module: 'suppliers',
-  },
-  {
-    label: 'Insumos',
-    path: 'supplies/view/all',
-    Icon: <PillIcon />,
-    name_module: 'supplies',
-  },
-  {
-    label: 'Cosechas',
-    path: 'harvests/view/all',
-    Icon: <Tractor />,
-    name_module: 'harvests',
-  },
-  {
-    label: 'Ventas',
-    path: 'sales/view/all',
-    Icon: <CircleDollarSign />,
-    name_module: 'sales',
-  },
-  {
-    label: 'Trabajos',
-    path: 'works/view/all',
-    Icon: <Pickaxe />,
-    name_module: 'works',
-  },
-  {
-    label: 'Pagos',
-    path: 'payments/view/all',
-    Icon: <CreditCard />,
-    name_module: 'payments',
-  },
-  {
-    label: 'Compras',
-    path: 'shopping/view/all',
-    Icon: <ShoppingBagIcon />,
-    name_module: 'sales',
-  },
-  {
-    label: 'Consumos',
-    path: 'consumption/view/all',
-    Icon: <Cable />,
-    name_module: 'supplies',
-  },
+  // {
+  //   label: 'Empleados',
+  //   path: 'employees/view/all',
+  //   Icon: <Contact />,
+  //   name_module: 'employees',
+  // },
+  // {
+  //   label: 'Cultivos',
+  //   path: 'crops/view/all',
+  //   Icon: <Leaf />,
+  //   name_module: 'crops',
+  // },
+  // {
+  //   label: 'Clientes',
+  //   path: 'clients/view/all',
+  //   Icon: <BookUser />,
+  //   name_module: 'clients',
+  // },
+  // {
+  //   label: 'Proveedores',
+  //   path: 'suppliers/view/all',
+  //   Icon: <Blocks />,
+  //   name_module: 'suppliers',
+  // },
+  // {
+  //   label: 'Insumos',
+  //   path: 'supplies/view/all',
+  //   Icon: <PillIcon />,
+  //   name_module: 'supplies',
+  // },
+  // {
+  //   label: 'Cosechas',
+  //   path: 'harvests/view/all',
+  //   Icon: <Tractor />,
+  //   name_module: 'harvests',
+  // },
+  // {
+  //   label: 'Ventas',
+  //   path: 'sales/view/all',
+  //   Icon: <CircleDollarSign />,
+  //   name_module: 'sales',
+  // },
+  // {
+  //   label: 'Trabajos',
+  //   path: 'works/view/all',
+  //   Icon: <Pickaxe />,
+  //   name_module: 'works',
+  // },
+  // {
+  //   label: 'Pagos',
+  //   path: 'payments/view/all',
+  //   Icon: <CreditCard />,
+  //   name_module: 'payments',
+  // },
+  // {
+  //   label: 'Compras',
+  //   path: 'shopping/view/all',
+  //   Icon: <ShoppingBagIcon />,
+  //   name_module: 'sales',
+  // },
+  // {
+  //   label: 'Consumos',
+  //   path: 'consumption/view/all',
+  //   Icon: <Cable />,
+  //   name_module: 'supplies',
+  // },
 ];
