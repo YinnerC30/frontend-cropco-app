520e388672bf06b6135e98181bfd1660c571ed27
Yinner Chilito <92528747+YinnerC30@users.noreply.github.com>
2024-11-26 09:22:04 -0500

    style: 💄 ButtonExportClients

--- src/modules/clients/components/ClientsModule/ButtonExportClients.tsx
+++ src/modules/clients/components/ClientsModule/ButtonExportClients.tsx
@@ -2,6 +2,7 @@ import { Button } from '@/components';
 import { FileUp } from 'lucide-react';
 import { useState } from 'react';
 import { useGetReportClients } from '../../hooks/useGetReportClients';
+import { ToolTipTemplate } from '@/modules/core/components';
 
 export const ButtonExportClients = () => {
   const [runQuery, setRunQuery] = useState(false);
@@ -23,8 +24,10 @@ export const ButtonExportClients = () => {
   });
 
   return (
-    <Button variant={'outline'} size={'icon'} onClick={handleClick}>
-      <FileUp className="w-4 h-4" />
-    </Button>
+    <ToolTipTemplate content="Exportar registros">
+      <Button variant={'outline'} size={'icon'} onClick={handleClick}>
+        <FileUp className="w-4 h-4" />
+      </Button>
+    </ToolTipTemplate>
   );
 };
