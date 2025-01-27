3ffa81207f6ba28976cd44d9b517529ecccbe244
Yinner Chilito <92528747+YinnerC30@users.noreply.github.com>
2025-01-27 12:27:26 -0500

    style: 💄 sonner and toaster

--- src/components/home/MainContent.tsx
+++ src/components/home/MainContent.tsx
@@ -1,7 +1,5 @@
 import { useSidebar } from '@/components/ui/sidebar';
 import { Outlet } from 'react-router-dom';
-import { Toaster as Sonner } from 'sonner';
-import { Toaster } from '../ui/toaster';
 
 export const MainContent: React.FC = () => {
   const { isMobile, open } = useSidebar();
@@ -18,8 +16,6 @@ export const MainContent: React.FC = () => {
         }`}
       >
         <Outlet />
-        <Sonner position="top-right" richColors closeButton />
-        <Toaster />
       </div>
     </main>
   );
