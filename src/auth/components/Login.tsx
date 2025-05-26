192343933b863fbb988c91cbd5dc98f7010c64a2
Yinner Chilito <92528747+YinnerC30@users.noreply.github.com>
2025-05-26 09:11:01 -0500

    refactor: 🛠️ mejorar la estructura y el diseño del componente de inicio de sesión, añadiendo un enlace a la página principal y ajustando el estilo del encabezado

--- src/auth/components/Login.tsx
+++ src/auth/components/Login.tsx
@@ -14,6 +14,7 @@ import { formFieldsLogin, formSchemaLogin } from '../utils';
 
 import { useCreateForm } from '@/modules/core/hooks';
 import { useState } from 'react';
+import { Link } from 'react-router-dom';
 import { useLoginUser } from '../hooks/mutations/useLoginUser';
 
 export const Login: React.FC = () => {
@@ -41,64 +42,73 @@ export const Login: React.FC = () => {
   const { email, password } = formFieldsLogin;
 
   return (
-    <div className="flex items-center justify-center h-screen max-w-sm mx-auto space-y-6">
-      <Card className="w-[350px]">
-        <CardHeader>
-          <CardTitle className="text-3xl font-bold text-center">
-            Inicio de sesión
-          </CardTitle>
-          <CardDescription className="text-center text-gray-500">
-            Ingresa tu usuario y contraseña para acceder a tu cuenta
-          </CardDescription>
-        </CardHeader>
-        <CardContent>
-          <Form {...formLogin}>
-            <form
-              onSubmit={formLogin.handleSubmit(onSubmit)}
-              id="formLogin"
-              className="flex flex-col gap-4 mt-4"
-            >
-              <FormFieldInput
-                control={formLogin.control}
-                description={email.description}
-                label={email.label}
-                name={'email'}
-                placeholder={email.placeholder}
-                disabled={false}
-              />
-
-              <FormFieldInput
-                control={formLogin.control}
-                description={password.description}
-                label={password.label}
-                name={'password'}
-                placeholder={password.placeholder}
-                disabled={false}
-                type={showPassword ? 'text' : 'password'}
+    <div className="flex flex-col h-screen gap-10 space-y-6">
+      <div className="px-4 py-4 border-b">
+        <Link to="/" className="text-2xl font-bold text-black-600">
+          CropCo
+        </Link>
+      </div>
+      <div className="flex items-center justify-center">
+        <Card className="w-[350px] ">
+          <CardHeader>
+            <CardTitle className="text-3xl font-bold text-center">
+              Inicio de sesión
+            </CardTitle>
+            <CardDescription className="text-center text-gray-500">
+              Ingresa tu usuario y contraseña para acceder a tu cuenta
+            </CardDescription>
+          </CardHeader>
+          <CardContent>
+            <Form {...formLogin}>
+              <form
+                onSubmit={formLogin.handleSubmit(onSubmit)}
+                id="formLogin"
+                className="flex flex-col gap-4 mt-4"
               >
-                <Button
-                  type="button"
-                  onClick={togglePasswordVisibility}
-                  className="p-2"
+                <FormFieldInput
+                  control={formLogin.control}
+                  description={email.description}
+                  label={email.label}
+                  name={'email'}
+                  placeholder={email.placeholder}
+                  disabled={false}
+                />
+
+                <FormFieldInput
+                  control={formLogin.control}
+                  description={password.description}
+                  label={password.label}
+                  name={'password'}
+                  placeholder={password.placeholder}
+                  disabled={false}
+                  type={showPassword ? 'text' : 'password'}
                 >
-                  {showPassword ? <EyeOpenIcon /> : <EyeClosedIcon />}
-                </Button>
-              </FormFieldInput>
-            </form>
-          </Form>
-        </CardContent>
-        <CardFooter className="flex justify-center gap-4">
-          <Button
-            type="submit"
-            form="formLogin"
-            className="mt-4 w-28"
-            disabled={isPending}
-          >
-            {isPending && <ReloadIcon className="w-4 h-4 mr-2 animate-spin" />}
-            Ingresar
-          </Button>
-        </CardFooter>
-      </Card>
+                  <Button
+                    type="button"
+                    onClick={togglePasswordVisibility}
+                    className="p-2"
+                  >
+                    {showPassword ? <EyeOpenIcon /> : <EyeClosedIcon />}
+                  </Button>
+                </FormFieldInput>
+              </form>
+            </Form>
+          </CardContent>
+          <CardFooter className="flex justify-center gap-4">
+            <Button
+              type="submit"
+              form="formLogin"
+              className="mt-4 w-28"
+              disabled={isPending}
+            >
+              {isPending && (
+                <ReloadIcon className="w-4 h-4 mr-2 animate-spin" />
+              )}
+              Ingresar
+            </Button>
+          </CardFooter>
+        </Card>
+      </div>
     </div>
   );
 };
--- src/components/landing/Header.tsx
+++ src/components/landing/Header.tsx
@@ -1,9 +1,10 @@
 import { PATH_LOGIN } from '@/config';
+import { cn } from '@/lib/utils';
 import { Link } from 'react-router-dom';
 
-export default function Header() {
+export default function Header({ className }: { className?: string }) {
   return (
-    <header className="shadow-sm ">
+    <header className={cn('shadow-sm', className)}>
       <div className="container flex items-center justify-between px-4 py-4 mx-auto">
         <Link to="/" className="text-2xl font-bold text-black-600">
           CropCo
