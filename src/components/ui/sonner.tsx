f1a1ae156ddf321ee4729cd84651ff5ff78ad293
Yinner Chilito <92528747+YinnerC30@users.noreply.github.com>
2025-05-31 08:55:27 -0500

    refactor: 🛠️ agregar la propiedad 'richColors' al componente Sonner en App y simplificar el componente Toaster, mejorando la presentación de las notificaciones

--- src/components/ui/sonner.tsx
+++ src/components/ui/sonner.tsx
@@ -1,4 +1,4 @@
-import { useTheme } from 'next-themes';
+import { useTheme } from '@/modules/core/components/shared/ThemeProvider';
 import { Toaster as Sonner } from 'sonner';
 
 type ToasterProps = React.ComponentProps<typeof Sonner>;
@@ -6,24 +6,7 @@ type ToasterProps = React.ComponentProps<typeof Sonner>;
 const Toaster = ({ ...props }: ToasterProps) => {
   const { theme = 'system' } = useTheme();
 
-  return (
-    <Sonner
-      theme={theme as ToasterProps['theme']}
-      className="toaster group"
-      toastOptions={{
-        classNames: {
-          toast:
-            'group toast group-[.toaster]:bg-background group-[.toaster]:text-foreground group-[.toaster]:border-border group-[.toaster]:shadow-lg',
-          description: 'group-[.toast]:text-muted-foreground',
-          actionButton:
-            'group-[.toast]:bg-primary group-[.toast]:text-primary-foreground',
-          cancelButton:
-            'group-[.toast]:bg-muted group-[.toast]:text-muted-foreground',
-        },
-      }}
-      {...props}
-    />
-  );
+  return <Sonner theme={theme as ToasterProps['theme']} {...props} />;
 };
 
 export { Toaster };
