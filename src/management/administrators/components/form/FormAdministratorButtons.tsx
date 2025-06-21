886d3c78e0dd0722c9cfca865793cfe802b4ca47
Yinner Chilito <92528747+YinnerC30@users.noreply.github.com>
2025-06-21 10:40:47 -0500

    refactor: ✨ integrar el contexto del formulario en FormAdministratorButtons, mejorando la gestión del estado de envío y la lógica de navegación

--- src/management/administrators/components/form/FormAdministratorButtons.tsx
+++ src/management/administrators/components/form/FormAdministratorButtons.tsx
@@ -3,9 +3,10 @@ import { ButtonsForm } from '@/modules/core/components';
 
 import { useNavigate } from 'react-router-dom';
 import { MODULE_ADMINISTRATORS_PATHS } from '../../routes/pathsRoutes';
+import { useFormAdministratorContext } from '../../hooks/context/useFormAdministratorContext';
 
 export const FormAdministratorButtons: React.FC = () => {
-  // const { readOnly, isSubmitting } = useFormAdministratorContext();
+  const { readOnly, isSubmitting } = useFormAdministratorContext();
 
   const navigate = useNavigate();
 
@@ -13,14 +14,14 @@ export const FormAdministratorButtons: React.FC = () => {
     navigate(MODULE_ADMINISTRATORS_PATHS.ViewAll);
   };
 
-  return false ? (
+  return readOnly ? (
     <Button className="my-2" onClick={handleReturnToModule}>
       Volver
     </Button>
   ) : (
     <ButtonsForm
       actionToCancel={handleReturnToModule}
-      isPending={false}
+      isPending={isSubmitting}
       formId="formAdministrator"
       className="flex w-48 gap-2 mt-2"
     />
