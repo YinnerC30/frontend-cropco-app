05449c2ae3ef07c44fe1a0d8272a47941c46fbb2
Yinner Chilito <92528747+YinnerC30@users.noreply.github.com>
2025-06-21 14:30:25 -0500

    refactor: ✨ actualizar el componente AdminUsers para integrar el formulario de inquilinos y mejorar la carga de datos de usuarios, además de extender las propiedades del componente FormTenant

--- src/management/tenants/components/administration-users/AdminUsers.tsx
+++ src/management/tenants/components/administration-users/AdminUsers.tsx
@@ -3,20 +3,27 @@ import { useParams } from 'react-router-dom';
 import { useGetAllTenantUsers } from '../../hooks/queries/useGetAllTenantUsers';
 import { MODULE_TENANTS_PATHS } from '../../routes/pathRoutes';
 import { TenantUsersTable } from './TenantUsersTable';
+import { FormTenant } from '../form';
+import { useGetTenant } from '../../hooks';
 
 export const AdminUsers = () => {
   const { id } = useParams();
 
-  const { data = [], isLoading } = useGetAllTenantUsers(id!);
+  const queryTenant = useGetTenant(id!);
 
-  if (isLoading) return <Loading />;
+  const queryTenantUsers = useGetAllTenantUsers(id!);
+
+  if (queryTenant.isLoading || queryTenantUsers.isLoading) return <Loading />;
+
   return (
     <div>
       <BreadCrumb
         items={[{ link: MODULE_TENANTS_PATHS.ViewAll, name: 'Inquilinos' }]}
         finalItem={'Administrar usuarios'}
       />
-      <TenantUsersTable data={data} />
+      <FormTenant defaultValues={queryTenant.data} readOnly={true}>
+        <TenantUsersTable data={queryTenantUsers.data || []} />
+      </FormTenant>
     </div>
   );
 };
--- src/management/tenants/components/form/FormTenant.tsx
+++ src/management/tenants/components/form/FormTenant.tsx
@@ -3,14 +3,24 @@ import { FormTenantButtons } from './FormTenantButtons';
 import { FormTenantProps, FormTenantProvider } from './FormTenantContext';
 import { FormTenantFields } from './FormTenantFields';
 
-export const FormTenant: React.FC<FormTenantProps> = (props) => {
+interface FormTenantPropsExtend extends FormTenantProps {
+  hiddenButtons?: boolean;
+  children?: React.ReactNode;
+}
+
+export const FormTenant: React.FC<FormTenantPropsExtend> = ({
+  hiddenButtons = false,
+  children,
+  ...props
+}) => {
   return (
     <FormTenantProvider {...props}>
       <div className="flex flex-col items-center">
         <ScrollArea className="h-[80vh] w-full pb-2">
           <FormTenantFields />
+          {children}
         </ScrollArea>
-        <FormTenantButtons />
+        {!hiddenButtons && <FormTenantButtons />}
       </div>
     </FormTenantProvider>
   );
