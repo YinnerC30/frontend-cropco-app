0ce0fc1dbf2e205b87fb534d739068c1c0943143
Yinner Chilito <92528747+YinnerC30@users.noreply.github.com>
2025-06-22 11:37:09 -0500

    refactor: ✨ eliminar importaciones innecesarias y limpiar el código en ColumnsTablePayments y ColumnsTablePaymentsPendingHarvest para mejorar la legibilidad

--- src/modules/payments/components/columns/ColumnsTablePaymentsPendingHarvest.tsx
+++ src/modules/payments/components/columns/ColumnsTablePaymentsPendingHarvest.tsx
@@ -3,7 +3,6 @@ import { ColumnDef } from '@tanstack/react-table';
 import { Badge } from '@/components';
 import { ButtonHeaderTable } from '@/modules/core/components';
 import { FormatDate } from '@/modules/core/helpers/formatting/FormatDate';
-import { FormatMoneyValue } from '@/modules/core/helpers/formatting/FormatMoneyValue';
 import { FormatNumber } from '@/modules/core/helpers/formatting/FormatNumber';
 import { HarvestDetail } from '@/modules/harvests/interfaces';
 
@@ -25,15 +24,6 @@ export const columnsPaymentsPendingHarvest: ColumnDef<HarvestDetail>[] = [
     accessorKey: 'value_pay',
     cell: ({ row }) => {
       return FormatNumber(row.getValue('value_pay'));
-    },
-    header: ({ column }: any) => {
-      return <ButtonHeaderTable column={column} label={'Valor a pagar'} />;
-    },
-  },
-  {
-    accessorKey: 'value_pay',
-    cell: ({ row }) => {
-      return FormatMoneyValue(row.getValue('value_pay'));
     },
     header: ({ column }: any) => {
       return <ButtonHeaderTable column={column} label={'Valor a pagar'} />;
