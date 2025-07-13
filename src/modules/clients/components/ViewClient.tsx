d7c741020cd237855c9b025ad480cee372114d30
Yinner Chilito <92528747+YinnerC30@users.noreply.github.com>
2025-07-13 14:27:29 -0500

    refactor: 🔄 reemplazar el componente genérico ClientDataTable por BasicDataTable en ViewClient y ViewEmployee, optimizando la reutilización del código y mejorando la estructura de las tablas de datos para una mejor visualización y gestión de la información.

--- src/modules/clients/components/ViewClient.tsx
+++ src/modules/clients/components/ViewClient.tsx
@@ -1,96 +1,23 @@
 import { useParams } from 'react-router-dom';
 
-import { ScrollArea, ScrollBar, Separator } from '@/components';
+import { Separator } from '@/components';
 import { Loading } from '@/modules/core/components';
 import { BreadCrumb } from '@/modules/core/components/';
-import {
-  FormDataTable,
-  FormDataTableButtonsPagination,
-  FormDataTableProvider,
-} from '@/modules/core/components/form/data-table';
-import { FormDataTablePageCount } from '@/modules/core/components/form/data-table/FormDataTablePageCount';
-import { FormDataTableRowCount } from '@/modules/core/components/form/data-table/FormDataTableRowCount';
-import { FormDataTableSelectPageSize } from '@/modules/core/components/form/data-table/FormDataTableSelectPageSize';
-import { useCreateColumnsTable } from '@/modules/core/hooks/data-table/useCreateColumnsTable';
-import { useDataTableGeneric } from '@/modules/core/hooks/data-table/useDataTableGeneric';
 import { useGetClient } from '../hooks/queries/useGetClient';
 import { MODULE_CLIENTS_PATHS } from '../routes/pathRoutes';
 import { FormClient } from './form';
 
+import { BasicDataTable } from '@/modules/core/components/form/basic/BasicDataTable';
 import { CircleDollarSign } from 'lucide-react';
 import { SaleDetailClient } from '../interfaces/SaleDetailClient';
 import { ActionsTableSaleDetailClient } from './form/actions/ActionsTableSaleDetailEmployee';
 import { columnsSaleDetailClient } from './form/columns/ColumnsTableSaleDetailEmployee';
 
-// Componente genérico para todas las tablas de empleados
-interface ClientDataTableProps<T> {
-  data: T[];
-  columns: any[];
-  actions: React.ComponentType<any>;
-  hiddenCheckbox?: boolean;
-  hiddenActions?: boolean;
-}
-
-export const ClientDataTable = <T extends Record<string, any>>({
-  data,
-  columns,
-  actions,
-  hiddenCheckbox = true,
-  hiddenActions = false,
-}: ClientDataTableProps<T>) => {
-  const columnsTable = useCreateColumnsTable<T>({
-    columns,
-    actions: actions as any,
-    hiddenCheckbox,
-    hiddenActions,
-  });
-
-  const dataTable = useDataTableGeneric<T>({
-    columns: columnsTable,
-    rows: data,
-  });
-
-  return (
-    <div>
-      <FormDataTableProvider
-        table={dataTable.table}
-        disabledDoubleClick={true}
-        errorMessage={'Ha ocurrido un error en la tabla'}
-        lengthColumns={dataTable.lengthColumns}
-      >
-        <div className="flex flex-col items-center justify-center w-screen gap-2 sm:w-full">
-          {/* Paginacion */}
-          <div className="flex flex-col items-center w-full gap-2 sm:flex-row sm:justify-evenly">
-            <FormDataTableRowCount />
-            <FormDataTableSelectPageSize />
-          </div>
-
-          {/* Tabla */}
-          <ScrollArea
-            className="h-max-[460px] w-[85%] sm:w-full p-1 border rounded-sm self-start"
-            type="auto"
-          >
-            <FormDataTable
-              onCellDoubleClick={(data) => {}}
-              disabledDoubleClick={true}
-            />
-
-            <ScrollBar className="mt-2" orientation="horizontal" forceMount />
-          </ScrollArea>
-
-          <FormDataTableButtonsPagination />
-          <FormDataTablePageCount />
-        </div>
-      </FormDataTableProvider>
-    </div>
-  );
-};
-
 // Componentes específicos usando el componente genérico
 export const ClientSaleDataTable: React.FC<{
   data: SaleDetailClient[];
 }> = ({ data }) => (
-  <ClientDataTable<SaleDetailClient>
+  <BasicDataTable<SaleDetailClient>
     data={data as any}
     columns={columnsSaleDetailClient}
     actions={ActionsTableSaleDetailClient}
--- src/modules/employees/components/ViewEmployee.tsx
+++ src/modules/employees/components/ViewEmployee.tsx
@@ -1,15 +1,4 @@
-import { ScrollArea, ScrollBar } from '@/components';
 import { BreadCrumb } from '@/modules/core/components/';
-import {
-  FormDataTable,
-  FormDataTableButtonsPagination,
-  FormDataTableProvider,
-} from '@/modules/core/components/form/data-table';
-import { FormDataTablePageCount } from '@/modules/core/components/form/data-table/FormDataTablePageCount';
-import { FormDataTableRowCount } from '@/modules/core/components/form/data-table/FormDataTableRowCount';
-import { FormDataTableSelectPageSize } from '@/modules/core/components/form/data-table/FormDataTableSelectPageSize';
-import { useCreateColumnsTable } from '@/modules/core/hooks/data-table/useCreateColumnsTable';
-import { useDataTableGeneric } from '@/modules/core/hooks/data-table/useDataTableGeneric';
 import { HarvestDetail } from '@/modules/harvests/interfaces';
 import { CreditCard, Pickaxe, Tractor } from 'lucide-react';
 import { useParams } from 'react-router-dom';
@@ -24,80 +13,17 @@ import { columnsWorkDetailEmployee } from './form/columns/ColumnsTableWorkDetail
 import FormEmployee from './form/FormEmployee';
 
 import { Tabs, TabsContent, TabsList, TabsTrigger } from '@/components/ui/tabs';
+import { BasicDataTable } from '@/modules/core/components/form/basic/BasicDataTable';
 import columnsPayment from '@/modules/payments/components/module/ColumnsTablePayment';
 import { Payment } from '@/modules/payments/interfaces/Payment';
 import { WorkDetailEmployee } from '../interfaces/WorkDetailEmployee';
 import { ActionsTablePaymentEmployee } from './form/actions/ActionsTablePaymentEmployee';
 
-// Componente genérico para todas las tablas de empleados
-interface EmployeeDataTableProps<T> {
-  data: T[];
-  columns: any[];
-  actions: React.ComponentType<any>;
-  hiddenCheckbox?: boolean;
-  hiddenActions?: boolean;
-}
-
-export const EmployeeDataTable = <T extends Record<string, any>>({
-  data,
-  columns,
-  actions,
-  hiddenCheckbox = true,
-  hiddenActions = false,
-}: EmployeeDataTableProps<T>) => {
-  const columnsTable = useCreateColumnsTable<T>({
-    columns,
-    actions: actions as any,
-    hiddenCheckbox,
-    hiddenActions,
-  });
-
-  const dataTable = useDataTableGeneric<T>({
-    columns: columnsTable,
-    rows: data,
-  });
-
-  return (
-    <div>
-      <FormDataTableProvider
-        table={dataTable.table}
-        disabledDoubleClick={true}
-        errorMessage={'Ha ocurrido un error en la tabla'}
-        lengthColumns={dataTable.lengthColumns}
-      >
-        <div className="flex flex-col items-center justify-center w-screen gap-2 sm:w-full">
-          {/* Paginacion */}
-          <div className="flex flex-col items-center w-full gap-2 sm:flex-row sm:justify-evenly">
-            <FormDataTableRowCount />
-            <FormDataTableSelectPageSize />
-          </div>
-
-          {/* Tabla */}
-          <ScrollArea
-            className="h-max-[460px] w-[85%] sm:w-full p-1 border rounded-sm self-start"
-            type="auto"
-          >
-            <FormDataTable
-              onCellDoubleClick={(data) => {}}
-              disabledDoubleClick={true}
-            />
-
-            <ScrollBar className="mt-2" orientation="horizontal" forceMount />
-          </ScrollArea>
-
-          <FormDataTableButtonsPagination />
-          <FormDataTablePageCount />
-        </div>
-      </FormDataTableProvider>
-    </div>
-  );
-};
-
 // Componentes específicos usando el componente genérico
 export const EmployeeHarvestDataTable: React.FC<{
   data: HarvestDetail[];
 }> = ({ data }) => (
-  <EmployeeDataTable<HarvestDetailEmployee>
+  <BasicDataTable<HarvestDetailEmployee>
     data={data as any}
     columns={columnsHarvestDetailEmployee}
     actions={ActionsTableHarvestDetailEmployee}
@@ -107,7 +33,7 @@ export const EmployeeHarvestDataTable: React.FC<{
 export const EmployeeWorkDataTable: React.FC<{
   data: WorkDetailEmployee[];
 }> = ({ data }) => (
-  <EmployeeDataTable<WorkDetailEmployee>
+  <BasicDataTable<WorkDetailEmployee>
     data={data}
     columns={columnsWorkDetailEmployee}
     actions={ActionsTableWorkDetailEmployee}
@@ -117,7 +43,7 @@ export const EmployeeWorkDataTable: React.FC<{
 export const EmployeePaymentDataTable: React.FC<{
   data: Payment[];
 }> = ({ data }) => (
-  <EmployeeDataTable<Payment>
+  <BasicDataTable<Payment>
     data={data}
     columns={columnsPayment}
     actions={ActionsTablePaymentEmployee}
