fe077ee692b433788b9a50bb0dcff1d60b379f23
Yinner Chilito <92528747+YinnerC30@users.noreply.github.com>
2025-07-15 08:00:15 -0500

    feat: ✨ actualizar configuración de Cypress para incluir un baseUrl y aumentar los timeouts, además de modificar la prueba de inicio de sesión para utilizar rutas relativas y verificar la visibilidad del mensaje de bienvenida.

--- cypress.config.ts
+++ cypress.config.ts
@@ -2,9 +2,18 @@ import { defineConfig } from 'cypress';
 
 export default defineConfig({
   e2e: {
+    baseUrl: 'http://prueba.localhost:5173',
+
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
     chromeWebSecurity: false,
+    // Aumenta los timeouts para conexiones lentas
+    defaultCommandTimeout: 10000,
+    requestTimeout: 10000,
+    responseTimeout: 10000,
   },
+  // hosts: {
+  //   'prueba.localhost': '127.0.0.1',
+  // },
 });
--- cypress/e2e/login-user.cy.ts
+++ cypress/e2e/login-user.cy.ts
@@ -1,5 +1,12 @@
 describe('template spec', () => {
   it('passes', () => {
-    cy.visit('http://prueba.localhost:5173');
+    cy.visit('/app/authentication/login');
+    cy.get('input[name="email"]').type('usermant@mail.com');
+    cy.get('input[name="password"]').type('123456');
+    cy.get('button[type="submit"]', { timeout: 5000 })
+      .should('be.visible')
+      .click();
+
+    cy.contains('Bienvenid@ a CropCo').should('be.visible');
   });
 });
