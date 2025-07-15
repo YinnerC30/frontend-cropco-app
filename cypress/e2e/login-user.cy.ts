ac51d4261041f41f1fa4f8b8b2dbec11b2fb5971
Yinner Chilito <92528747+YinnerC30@users.noreply.github.com>
2025-07-15 13:05:55 -0500

    feat: ✨ eliminar archivo de prueba demo.cy.ts y agregar nuevas pruebas en login-user.cy.ts para validar el manejo de errores con credenciales inválidas y formularios vacíos.

--- cypress/e2e/login-user.cy.ts
+++ cypress/e2e/login-user.cy.ts
@@ -9,4 +9,26 @@ describe('Login de usuario', () => {
 
     cy.contains('Bienvenid@ a CropCo').should('be.visible');
   });
+
+  it('debería mostrar error con credenciales inválidas', () => {
+    cy.visit('/app/authentication/login');
+    cy.get('input[name="email"]').type('otrocorreo@mail.com');
+    cy.get('input[name="password"]').type('contraseñaIncorrecta');
+    cy.get('button[type="submit"]', { timeout: 5000 })
+      .should('be.visible')
+      .click();
+
+    cy.contains('Usuario o contraseña incorrectos, inténtelo nuevamente').should('be.visible');
+  });
+
+  it('debería mostrar errores al intentar enviar el formulario vacío', () => {
+    cy.visit('/app/authentication/login');
+    cy.get('button[type="submit"]', { timeout: 5000 })
+      .should('be.visible')
+      .click();
+
+    cy.contains('El correo electrónico no es valido').should('be.visible');
+    cy.contains('Debes ingresar la contraseña').should('be.visible');
+    cy.contains('Faltan campos por rellenar en el formulario').should('be.visible');
+  });
 });
