8fe9864c7e864c0e7717036d0fb5f2e6ea935326
Yinner Chilito <92528747+YinnerC30@users.noreply.github.com>
2025-07-16 10:51:50 -0500

    feat: ✨ reestructurar pruebas de modificación de usuarios, eliminando pruebas duplicadas y mejorando la verificación del estado del switch global de acciones para una mejor legibilidad y funcionalidad.

--- cypress/e2e/users/users-module.cy.ts
+++ cypress/e2e/users/users-module.cy.ts
@@ -27,38 +27,6 @@ describe('Modulo de usuarios', () => {
     cy.existPaginationButtons();
   });
 
-  it('Debe crear un usuario ', () => {
-    cy.wait(3000);
-    cy.clickOnCreateButton();
-    cy.getFormInput('first_name').type('UserName');
-    cy.getFormInput('last_name').type('LastName');
-    const randomNum = Math.floor(10 + Math.random() * 90);
-    cy.getFormInput('email').type(`emailtest${randomNum}@gmail.com`);
-    cy.getFormInput('cell_phone_number').type('3123456547');
-    cy.getFormInput('passwords.password1').type('123456');
-    cy.getFormInput('passwords.password2').type('123456');
-
-    cy.clickOnSubmitButton();
-    cy.checkDisabledSubmitButton();
-    cy.contains('Usuario creado');
-  });
-
-  it('Modificar usuario existente', () => {
-    cy.createUser({}).then((email) => {
-      cy.visit(`/app/home/users/view/all?query=${email}`);
-      cy.get('button[data-testid="btn-actions-table"]').click();
-      cy.get('a[data-testid="link-update-record"]').click();
-      cy.getFormInput('first_name').clear().type('UserNameChanged');
-      cy.getFormInput('last_name').clear().type('LastNameChanged');
-      const randomNum = Math.floor(10 + Math.random() * 90);
-      cy.getFormInput('email').clear().type(`emailtest${randomNum}@gmail.com`);
-      cy.getFormInput('cell_phone_number').clear().type('3123451111');
-      cy.clickOnSubmitButton();
-      cy.checkDisabledSubmitButton();
-      cy.contains('Usuario actualizado');
-    });
-  });
-
   it('Eliminar usuario', () => {
     cy.createUser({}).then((email) => {
       cy.visit(`/app/home/users/view/all?query=${email}`);
@@ -276,5 +244,30 @@ describe('Creación de usuarios', () => {
     //   // cy.log(JSON.stringify(interception.response.body));
     // });
     // cy.wait(2000)
+  });
+});
+
+describe.only('Modificación de usuarios', () => {
+  beforeEach(() => {
+    cy.loginUser();
+    cy.navigateToModuleWithSideBar('users');
+  });
+
+  it('Modificar usuario existente', () => {
+    cy.createUser({}).then((email) => {
+      cy.visit(`/app/home/users/view/all?query=${email}`);
+      cy.get('button[data-testid="btn-actions-table"]').click();
+      cy.get('a[data-testid="link-update-record"]').click();
+      cy.getFormInput('first_name').clear().type('UserNameChanged');
+      cy.getFormInput('last_name').clear().type('LastNameChanged');
+      const randomNum = Math.floor(10 + Math.random() * 90);
+      cy.getFormInput('email').clear().type(`emailtest${randomNum}@gmail.com`);
+      cy.getFormInput('cell_phone_number').clear().type('3123451111');
+      cy.checkGlobalActionsSwitchState(false); // Verifica que está activo
+      cy.clickGlobalActionsSwitch();
+      cy.clickOnSubmitButton();
+      cy.checkDisabledSubmitButton();
+      cy.contains('Usuario actualizado');
+    });
   });
 });
