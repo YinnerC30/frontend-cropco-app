f745895561b1f265b25e8a1dd3b1ccf435069d9e
Yinner Chilito <92528747+YinnerC30@users.noreply.github.com>
2025-07-20 08:54:14 -0500

    feat: ✨ agregar pruebas para el módulo de empleados
    Se ha creado un nuevo archivo de pruebas para el módulo de empleados que incluye casos para verificar la navegación, la existencia de elementos, la funcionalidad de recarga de datos y la selección de elementos en la tabla. Además, se ha mejorado la lógica de habilitación del botón de recarga en el componente de acciones de empleados para desactivarlo durante la carga de datos.

---
+++ cypress/e2e/employees/employees-module.cy.ts
@@ -0,0 +1,88 @@
+describe('Modulo de empleados', () => {
+  beforeEach(() => {
+    cy.loginUser();
+    cy.navigateToModuleWithSideBar('employees');
+  });
+
+  it('Debe ingresar a la ruta correcta ', () => {
+    cy.contains('Usuarios');
+    cy.checkCurrentUrl('employees/view/all');
+  });
+
+  it('Debe contener los elementos necesarios', () => {
+    cy.existBasicSearchBar();
+
+    cy.existRefetchButton();
+    cy.existCreateButton();
+
+    cy.existPaginationInfo();
+
+    cy.contains('Nombre:');
+    cy.contains('Apellido:');
+    cy.contains('Correo electrónico:');
+    cy.contains('Número celular:');
+    cy.contains('Dirección:');
+
+    cy.existPaginationButtons();
+  });
+
+  it.only('Debe mostrar el loading cuando se intenta forzar la recarga de datos', () => {
+    cy.clickRefetchButton();
+    cy.checkRefetchButtonState(false);
+    cy.contains('Cargando información');
+  });
+
+  it('Se puede seleccionar todos los elementos al dar clic sobre el checkbox del encabezado', () => {
+    cy.wait(2000);
+    cy.get('button[data-testid="btn-clear-selection-table"]').should(
+      'not.be.visible'
+    );
+    cy.get('button[data-testid="btn-delete-bulk"]').should('not.be.visible');
+    cy.get('button[aria-label="Select all"]').click();
+    cy.get('span[data-testid="data-table-row-selection-number"]')
+      .invoke('text')
+      .then((text) => {
+        const value = Number(text.trim());
+        expect(value).to.be.greaterThan(0);
+      });
+    cy.get('button[data-testid="btn-clear-selection-table"]').should(
+      'be.visible'
+    );
+    cy.get('button[data-testid="btn-delete-bulk"]').should('be.visible');
+  });
+
+  it('Debe deseleccionar todos los elementos al dar clic nuevamente en el checkbox del encabezado', () => {
+    cy.wait(2000);
+    cy.get('button[aria-label="Select all"]').click(); // Selecciona todos
+    cy.get('span[data-testid="data-table-row-selection-number"]')
+      .invoke('text')
+      .then((text) => {
+        const value = Number(text.trim());
+        expect(value).to.be.greaterThan(0);
+      });
+    cy.get('button[aria-label="Select all"]').click(); // Deselecciona todos
+    cy.get('button[data-testid="btn-clear-selection-table"]').should(
+      'not.be.visible'
+    );
+    cy.get('button[data-testid="btn-delete-bulk"]').should('not.be.visible');
+    cy.get('span[data-testid="data-table-row-selection-number"]')
+      .invoke('text')
+      .then((text) => {
+        const value = Number(text.trim());
+        expect(value).to.equal(0);
+      });
+  });
+
+  it('Ingresar al modulo usando el command', () => {
+    cy.visit('/app/home/page');
+    cy.wait(3000);
+    cy.get('body').type('{ctrl}j');
+    cy.get('input[data-testid="input-command-search"]').type('empleados');
+    cy.get('div[data-testid="command-item-employees"]').click();
+    cy.checkCurrentUrl('employees/view/all');
+  });
+
+  //TODO: Ingresar usuario con permisos y verificar que esten visibles y disponibles
+  //TODO: Probar selección
+  //TODO: Probar orden de datos en las tablas
+});
