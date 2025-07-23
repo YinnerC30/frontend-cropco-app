81122cf10afaab11bb9fca52e91022c300c63e79
Yinner Chilito <92528747+YinnerC30@users.noreply.github.com>
2025-07-23 16:02:43 -0500

    feat: ✨ actualizar la lógica de identificación en el componente ActionsTablePaymentsToPay
    Se ha modificado la función getCorrectId para utilizar registros de pagos en lugar de detalles de cosechas y trabajos, mejorando la flexibilidad del componente. Además, se han añadido nuevas propiedades opcionales en la interfaz Payment para incluir pagos de cosechas y trabajos, lo que amplía la capacidad de gestión de datos en el sistema.

--- src/modules/payments/components/columns/ActionsTablePaymentsToPay.tsx
+++ src/modules/payments/components/columns/ActionsTablePaymentsToPay.tsx
@@ -4,15 +4,13 @@ import {
   ActionNavigate,
   DropDownMenuActions,
 } from '@/modules/core/components';
-import { Row } from '@tanstack/react-table';
-import { toast } from 'sonner';
-import { RecordToPay } from '../../interfaces/RecordToPay';
-import { useFormPaymentContext } from '../../hooks/context/useFormPaymentContext';
-import { ShieldCheck } from 'lucide-react';
 import { MODULE_HARVESTS_PATHS } from '@/modules/harvests/routes/pathRoutes';
 import { MODULE_WORKS_PATHS } from '@/modules/work/routes/pathRoutes';
-import { WorkDetail } from '@/modules/work/interfaces/WorkDetail';
-import { HarvestDetail } from '@/modules/harvests/interfaces';
+import { Row } from '@tanstack/react-table';
+import { ShieldCheck } from 'lucide-react';
+import { toast } from 'sonner';
+import { useFormPaymentContext } from '../../hooks/context/useFormPaymentContext';
+import { RecordToPay } from '../../interfaces/RecordToPay';
 
 export const ActionsTablePaymentsToPay = ({
   row,
@@ -34,15 +32,15 @@ export const ActionsTablePaymentsToPay = ({
 
   const getCorrectId = (id: string, typeRecord: string) => {
     if (typeRecord === 'harvest') {
-      const harvestDetail = paymentsState.original_data?.harvests_detail?.find(
-        (item: HarvestDetail) => item.id === id
+      const record: any = paymentsState.records_to_pay?.find(
+        (item: any) => item.id === id
       );
-      return harvestDetail?.harvest?.id || '';
+      return record?.harvest?.id || '';
     } else {
-      const workDetail = paymentsState.original_data?.works_detail?.find(
-        (item: WorkDetail) => item.id === id
+      const record: any = paymentsState.records_to_pay.find(
+        (item: any) => item.id === id
       );
-      return workDetail?.work?.id || '';
+      return record?.work?.id || '';
     }
   };
 
