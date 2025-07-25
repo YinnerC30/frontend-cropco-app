4b61ad9326ae21e6ae7b13b8b3d73ac5279561c3
Yinner Chilito <92528747+YinnerC30@users.noreply.github.com>
2025-07-25 13:45:37 -0500

    feat: ✨ mejorar mensaje de error en la eliminación de cultivos
    Se ha actualizado el mensaje de error en la función `useDeleteCrop` para proporcionar una descripción más clara sobre las condiciones que impiden la eliminación de un cultivo, especificando la necesidad de verificar el stock disponible y las ventas con pagos pendientes.

--- src/modules/crops/hooks/mutations/useDeleteCrop.ts
+++ src/modules/crops/hooks/mutations/useDeleteCrop.ts
@@ -29,7 +29,7 @@ export const useDeleteCrop = (): UseMutationReturn<void, string> => {
         handlers: {
           conflict: {
             message:
-              'No se pudo eliminar el cultivo seleccionado, revisa que no tenga stock',
+              'No fue posible eliminar el cultivo seleccionado. Verifica que no tenga stock disponible ni ventas con pagos pendientes antes de intentar eliminarlo.',
           },
         },
       });
